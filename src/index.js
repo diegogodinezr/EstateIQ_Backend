7202344c90fe2dada50ac49df642f03d0b323838
diegogodinezr <godinezroblesdiego@gmail.com>
2024-10-01 19:10:45 -0600

    4

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import {connectDB} from './database.js';
 
 connectDB();
 app.listen(3000)
-console.log('Server on port', 3000);
+// console.log('Server on port', 3000);
+console.log('Server running');
 
