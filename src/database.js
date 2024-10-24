e905e6b5442b85bc8bf2ff0fbb2aa6fe16566ca0
diegogodinezr <godinezroblesdiego@gmail.com>
2024-10-23 19:56:29 -0600

    secret

--- src/database.js
+++ src/database.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Cargar variables de entorno
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://diegogodinezr:diego123@cluster0.hgwcvxq.mongodb.net/');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB is Connected...');
   } catch (err) {
     console.error(err.message);
